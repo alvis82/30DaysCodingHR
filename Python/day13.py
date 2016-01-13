79835fe8db236759b671d24eeeb3a28109d0cb97
alvis82 <alvis82@gmail.com>
2016-01-13 10:54:09 -0500

    Add solutions for Day 13: Abstract Classes!

---
+++ Python/day13.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+
+class Book:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display(): pass
+
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        Book.__init__(self, title, author)
+        self.price = price
+
+    def display(self):
+        print "Title: {}\nAuthor: {}\nPrice: {}".format(title, author, price)
+
+title = raw_input()
+author = raw_input()
+price = int(raw_input())
+MyBook(title,author,price).display()
