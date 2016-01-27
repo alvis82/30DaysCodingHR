0cb87b6815bec4b4509992c020fe27900cd35701
alvis82 <alvis82@gmail.com>
2016-01-27 11:08:51 -0500

    Add solutions for Day 27: Testing Part II!

---
+++ C++/day27.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "5" << endl;
+    cout << "3 3" << endl;
+    cout << "-1 0 1" << endl;
+    cout << "4 2" << endl;
+    cout << "-1 0 1 2" << endl;
+    cout << "5 4" << endl;
+    cout << "-2 -1 0 1 2" << endl;
+    cout << "6 3" << endl;
+    cout << "-2 -1 0 1 2 3" << endl;
+    cout << "7 4" << endl;
+    cout << "-1 0 1 2 3 4 5" << endl;
+
+    return 0;
+}
---
+++ Java/day27.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("5");
+        System.out.println("3 3");
+        System.out.println("-1 0 1");
+        System.out.println("4 2");
+        System.out.println("-1 0 1 2");
+        System.out.println("5 4");
+        System.out.println("-2 -1 0 1 2");
+        System.out.println("6 3");
+        System.out.println("-2 -1 0 1 2 3");
+        System.out.println("7 4");
+        System.out.println("-1 0 1 2 3 4 5");
+    }
+}
